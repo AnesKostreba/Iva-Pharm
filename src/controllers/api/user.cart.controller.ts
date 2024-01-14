996892446eec75854c1365853f766dd94fa9918f
AnesKostreba <aneskostreba@yahoo.com>
2024-01-14 08:47:25 +0100

    Implement the mechanism to make orders

--- src/controllers/api/user.cart.controller.ts
+++ src/controllers/api/user.cart.controller.ts
@@ -7,10 +7,16 @@ import { Request } from "express";
 import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { AddArticleToCartDto } from "src/dtos/cart/add.article.to.cart.dto";
 import { EditArticleInCartDto } from "src/dtos/cart/edit.article.in.cart.dto";
+import { Order } from "src/entities/order.entity";
+import { OrderService } from "src/services/order/order.service";
+import { ApiResponse } from "src/misc/api.response.class";
 
 @Controller('api/user/cart')
 export class UserCartController{
-    constructor(private cartService: CartService){}
+    constructor(
+        private cartService: CartService,
+        private orderService: OrderService
+        ){}
 
     private async getActiveCartForUserId(userId: number):Promise<Cart>{
         
@@ -50,5 +56,14 @@ export class UserCartController{
         const cart = await this.getActiveCartForUserId(req.token.id);
 
         return await this.cartService.changeQuantity(cart.cartId, data.articleId, data.quantity);
+    }
+
+    // Post http://localhost:3000/api/user/cart/makeOrder/
+    @Post('makeOrder')
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('user')
+    async makeOrder( @Req() req: Request):Promise<Order | ApiResponse>{
+        const cart = await this.getActiveCartForUserId(req.token.id);
+        return await this.orderService.add(cart.cartId);
     }
 }
