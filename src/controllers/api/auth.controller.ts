2af7b8ead364bc8d6f20693b6dd4cac9b29b8c98
AnesKostreba <aneskostreba@yahoo.com>
2024-01-15 20:41:14 +0100

    Implement refresh token mechanism for the user

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put, Req} from "@nestjs/common";
+import { Body, Controller, HttpException, HttpStatus, Post, Put, Req} from "@nestjs/common";
 import { LoginAdministratorDto } from "src/dtos/administrator/login.administrator.dto";
 import { ApiResponse } from "src/misc/api.response.class";
 import { AdministratorService } from "src/services/administrator/administrator.service";
@@ -11,6 +11,8 @@ import { jwtSecret } from "config/jwt.secret";
 import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { UserService } from "src/services/user/user.service";
 import { LoginUserDto } from "src/dtos/user/login.user.dto";
+import { JwtRefreshDataDto } from "src/dtos/auth/jwt.refresh.dto";
+import { UserRefreshTokenDto } from "src/dtos/auth/user.refresh.token.dto";
 
 @Controller('auth/')
 export class AuthController {
@@ -49,10 +51,7 @@ export class AuthController {
         jwtData.id = administrator.administratorId;
         jwtData.identity = administrator.username;
 
-        let sada = new Date();
-        sada.setDate(sada.getDate() + 14);
-        const istekTimeStamp = sada.getTime() / 1000.0;
-        jwtData.exp = istekTimeStamp;
+        jwtData.exp = this.getDatePlus(60 * 60 * 24 * 14); // 14 dana;
 
         jwtData.ip = req.ip.toString();
         jwtData.ua = req.headers["user-agent"];
@@ -62,7 +61,9 @@ export class AuthController {
         const responseObject = new LoginInfoDto(
             administrator.administratorId,
             administrator.username,
-            token
+            token,
+            "",
+            ""
         ) ;
 
         return new Promise(resolve => resolve(responseObject))
@@ -97,23 +98,114 @@ export class AuthController {
         jwtData.id = user.userId;
         jwtData.identity = user.email;
 
-        let sada = new Date();
-        sada.setDate(sada.getDate() + 14);
-        const istekTimeStamp = sada.getTime() / 1000.0;
-        jwtData.exp = istekTimeStamp;
+        jwtData.exp = this.getDatePlus(60 * 5); // 5 min token traje
 
         jwtData.ip = req.ip.toString();
         jwtData.ua = req.headers["user-agent"];
 
         let token: string =  jwt.sign(jwtData.toPlainObject(), jwtSecret); //GEN !!
 
+        const jwtRefresData = new JwtRefreshDataDto();
+        jwtRefresData.role = jwtData.role;
+        jwtRefresData.id = jwtData.id;
+        jwtRefresData.identity = jwtData.identity;
+        jwtRefresData.exp = this.getDatePlus(60 * 60 * 24 * 31);// koliko sekundi od sada...31 dan
+        jwtRefresData.ip = jwtData.ip;
+        jwtRefresData.ua = jwtData.ua;
+
+        let refreshToken: string =  jwt.sign(jwtRefresData.toPlainObject(), jwtSecret); //GEN !!
+
         const responseObject = new LoginInfoDto(
             user.userId,
             user.email,
-            token
+            token,
+            refreshToken,
+            this.getIsoDate(jwtRefresData.exp)
         ) ;
+
+        await this.userService.addToken(
+            user.userId, 
+            refreshToken, 
+            this.getDatabaseDateFormat(this.getIsoDate(jwtRefresData.exp))
+        );
 
         return new Promise(resolve => resolve(responseObject))
 
+    }
+    @Post('user/refresh') //http://localhost:3000/auth/user/refresh
+    async userTokenRefresh(@Req() req: Request, @Body() data: UserRefreshTokenDto):Promise<LoginInfoDto | ApiResponse> {
+        const userToken = await this.userService.getUserToken(data.token)
+
+        if(!userToken){
+            return new ApiResponse('error', -10002, 'No such refresh token!')
+        }
+
+        if(userToken.isValid === 0){
+            return new ApiResponse('error', -10003, 'The token is no longer valid!')
+        }
+
+        const sada = new Date();
+        const datumIsteka = new Date(userToken.expiresAt);
+
+        if(datumIsteka.getTime() < sada.getTime()){
+            return new ApiResponse('error', -10004, 'The token has expired!!')
+        }
+
+        let jwtRefreshData: JwtRefreshDataDto;
+
+        try {
+            jwtRefreshData = jwt.verify(data.token, jwtSecret);
+        } catch (e) {
+            throw new HttpException('Bad token found!', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (!jwtRefreshData) {
+            throw new HttpException('Bad token found!', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (jwtRefreshData.ip !== req.ip.toString()) {
+            throw new HttpException('Bad token found!', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (jwtRefreshData.ua !== req.headers["user-agent"]) {
+            throw new HttpException('Bad token found!', HttpStatus.UNAUTHORIZED);
+        }
+
+        const jwtData = new JwtDataDto();
+        jwtData.role = jwtRefreshData.role;
+        jwtData.id = jwtRefreshData.id;
+        jwtData.identity = jwtRefreshData.identity;
+
+        jwtData.exp = this.getDatePlus(60 * 5); // 5 min token traje
+
+        jwtData.ip = jwtRefreshData.ip;
+        jwtData.ua = jwtRefreshData.ua;
+
+        let token: string =  jwt.sign(jwtData.toPlainObject(), jwtSecret); //GEN !!
+
+        const responseObject = new LoginInfoDto(
+            jwtData.id,
+            jwtData.identity,
+            token,
+            data.token,
+            this.getIsoDate(jwtRefreshData.exp)
+        ) ;
+
+        return responseObject;
+    }
+
+    private getDatePlus(numberOfSecounds: number):number{
+        // trenutni datum, trenutno vreme tog datuma preracunatih u sekunde i dodati broj sekundi
+        return new Date().getTime() / 1000.0  + numberOfSecounds; 
+    }
+
+    private getIsoDate(timestamp: number):string{
+        const date = new Date();
+        date.setTime(timestamp * 1000);
+        return date.toISOString();
+    }
+
+    private getDatabaseDateFormat(isoFormat: string):string {
+        return isoFormat.substring(0,19).replace('T', ' ');
     }
 }
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -6,12 +6,16 @@ import { User } from "src/entities/user.entity";
 import { ApiResponse } from "src/misc/api.response.class";
 import { Repository } from "typeorm";
 import * as crypto from 'crypto'
+import { UserToken } from "src/entities/user-token.entity";
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<User>{
     constructor(
         @InjectRepository(User)
-        private readonly user: Repository<User>
+        private readonly user: Repository<User>, 
+
+        @InjectRepository(UserToken)
+        private readonly userToken: Repository<UserToken>
     ) {
         super(user)
     }
@@ -55,5 +59,50 @@ export class UserService extends TypeOrmCrudService<User>{
             return user;
         }
         return null;
+    }
+
+    async addToken(userId: number, token: string, exiresAt: string){
+        const userToken = new UserToken();
+        userToken.userId = userId;
+        userToken.token = token;
+        userToken.expiresAt = exiresAt;
+
+        return await this.userToken.save(userToken);
+    }
+
+    async getUserToken(token: string):Promise<UserToken>{ // po njegovom stringu ga mozemo jedino preoznati da bismo ga izvukli
+        return await this.userToken.findOne({
+            where:{token: token}
+        })
+    }
+
+    async invalidateToken(token: string):Promise<UserToken | ApiResponse>{ // invalidacija tokena
+        const userToken = await this.userToken.findOne({
+            where:{token: token}
+        });
+
+        if(!userToken){
+            return new ApiResponse('error', -10001, 'No such refresh token!')
+        }
+
+        userToken.isValid = 0;
+
+        await this.userToken.save(userToken);
+
+        return await this.getUserToken(token);
+    }
+
+    async invalidateUserTokens(userId: number): Promise<(UserToken | ApiResponse)[]>{
+        const userTokens = await this.userToken.find({
+            where:{ userId : userId}
+        });
+
+        const results = [];
+
+        for(const userToken of userTokens){
+            results.push(this.invalidateToken(userToken.token));
+        }
+
+        return results;
     }
 }
